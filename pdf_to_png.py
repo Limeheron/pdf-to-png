f1182f388e47475a64c0dc2396fd1a7384abc81a
Limeheron <155134245+Limeheron@users.noreply.github.com>
2024-01-23 15:20:09 +0900

    pdf2png.py to pdf_to_png.py

--- pdf_to_png.py
+++ pdf_to_png.py
@@ -1,7 +1,7 @@
 import os, sys
 import fitz
 
-def PDF_to_PNG(file_path):
+def pdf_to_png(file_path):
     file_name = os.path.splitext(os.path.basename(file_path))[0]
     folder_path = os.path.dirname(file_path)
     page_path = os.path.join(folder_path, file_name)
@@ -13,21 +13,21 @@ def PDF_to_PNG(file_path):
         os.makedirs(image_path)
     print("Output Folder Created")
 
-    Pages_to_PNG(file_path, page_path)
-    PDF_Image_Extract(file_path, image_path)
+    pages_to_png(file_path, page_path)
+    pdf_image_extract(file_path, image_path)
     print(f"Saved in {page_path}")
-    
+
 # 페이지 전체 추출
-def Pages_to_PNG(file_path, output_path):
+def pages_to_png(file_path, output_path):
     docs = fitz.open(file_path)
     for page in docs:
         pix = page.get_pixmap(matrix=fitz.Matrix(4, 4))     # 4배 해상도
         output = os.path.join(output_path, "p_%i.png" % page.number)
         pix.save(output)
-    print("All Pages Saved")
+    print("페이지 저장 완료")
 
 # 모든 이미지 추출
-def PDF_Image_Extract(file_path, output_path):
+def pdf_image_extract(file_path, output_path):
     docs = fitz.open(file_path)
     for pages in range(docs.page_count):
         page = docs[pages]
@@ -42,8 +42,7 @@ def PDF_Image_Extract(file_path, output_path):
             full_path = os.path.join(output_path, image_filename)
             with open(full_path, "wb") as image_file:
                 image_file.write(image_bytes)
-
-    print("All Image Extracted")
+    print("이미지 추출 완료")
     docs.close()
 
 if __name__ == "__main__":
@@ -57,4 +56,4 @@ if __name__ == "__main__":
         print("Error: File not found")
         sys.exit(1)
 
-    PDF_to_PNG(file_path)
+    pdf_to_png(file_path)
